e5c8d9f1f54e6381aa1b539cc81de90f1d908552
Toni <toni.pasqual@gmail.com>
2020-11-09 15:46:32 +0100

    components 2

---
+++ components/myfooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyFooter = () => {
+    return (
+        <div>
+            <p>footer</p>
+        </div>
+    )
+}
+
+export default MyFooter
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyFooter from '../components/myfooter';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hola</h1>
+            <h2>Prueba 2</h2>
+            <MyFooter />
+        </div>
+    )
+}
+
+export default Home
